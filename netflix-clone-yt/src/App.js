f5166a3ba9741f356756f0e462dd0a22d693d124
Ramana8801 <90092732+Ramana8801@users.noreply.github.com>
2021-10-31 18:31:56 +0530

    initi

---
+++ netflix-clone-yt/src/App.js
@@ -0,0 +1,37 @@
+import React,{useEffect} from 'react';
+import { useSelector,useDispatch  } from 'react-redux';
+import './App.css';
+import HomeScreen from './css/HomeScreen';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
+import Login from './login';
+
+function App() {
+  const user=useSelector(selectUser);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe =auth.onAuthStateChanged(user=>{
+      if(user){
+        dispatch(login({
+          uid:user.uid,
+          email:user.email
+        }))
+      }else{
+        dispatch(logout)
+      }
+    })
+    return unsubscribe;
+  }, [])
+  return (
+    <div className="App">
+      {
+        !user ?(<Login/>):
+        (<HomeScreen/>)
+      }
+     
+    </div>
+  );
+}
+
+export default App;
